4a620ac10845ca50889c5b70010f55de8a669006
juanf-radical <75054897+juanf-radical@users.noreply.github.com>
2021-07-19 09:08:02 -0500

    nuevos layouts y actualiza backlog

---
+++ app/src/main/java/com/example/appmovil/Detalle_Solicitud.java
@@ -0,0 +1,14 @@
+package com.example.appmovil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Detalle_Solicitud extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_solicitud);
+    }
+}
---
+++ app/src/main/java/com/example/appmovil/Olvido_Password.java
@@ -0,0 +1,14 @@
+package com.example.appmovil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Olvido_Password extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_olvido_password);
+    }
+}
