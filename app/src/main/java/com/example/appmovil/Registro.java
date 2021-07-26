41532bb74473e33e0901afa2be7720d68a9fd515
juanf-radical <75054897+juanf-radical@users.noreply.github.com>
2021-07-26 09:46:39 -0500

    layout perfil y diseño solicitud

--- app/src/main/java/com/example/appmovil/Registro.java
+++ app/src/main/java/com/example/appmovil/Registro.java
@@ -3,19 +3,32 @@ package com.example.appmovil;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Registro extends AppCompatActivity implements View.OnClickListener {
     private Button btRegistro;
+    private EditText editTextTextRusuario, editTextTextRpassword, editTextTextRepPass, editTextTextNombre,editTextTextSexo, editTextTextFechadeNacimiento, editTextTextDireccion;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
         Button btRegistro = findViewById(R.id.btRegistro);
+        EditText editTextTextRusuario = findViewById(R.id.editTextTextRusuario);
+        EditText editTextTextRpassword = findViewById(R.id.editTextTextRpassword);
+        EditText editTextTextRepPass = findViewById(R.id.editTextTextRepPass);
+        EditText editTextTextNombre = findViewById(R.id.editTextTextNombre);
+        EditText editTextTextSexo = findViewById(R.id.editTextTextSexo);
+        EditText editTextTextFechadeNacimiento = findViewById(R.id.editTextTextFechadeNacimiento);
+        EditText editTextTextDireccion = findViewById(R.id.editTextTextDireccion);
         btRegistro.setOnClickListener(this);
     }
 
@@ -29,4 +42,15 @@ public class Registro extends AppCompatActivity implements View.OnClickListener 
         }
 
     }
+    //public void grabar (View v){
+        //String usuario = editTextTextRusuario.getText().toString();
+        //String password = editTextTextRpassword.getText().toString();
+        //String password2 = editTextTextRepPass.getText().toString();
+        //String nombre =editTextTextNombre.getText().toString();
+        //String sexo = editTextTextSexo.getText().toString();
+        //String fecha = editTextTextFechadeNacimiento.getText().toString();
+        //String direccion = editTextTextDireccion.getText().toString();
+        //SharedPreferences preferencias = getSharedPreferences("datoscontato",Context.MODE_PRIVATE);
+        //SharedPreferences.Editor editor = preferencias.edit();
+        //editor.putString();
 }
