593b2a09652b8b65f6fb0b815d9628340de4aabf
Jose Javier <josejaviermu@gmail.com>
2021-08-17 22:41:42 -0500

    login con Google

--- app/src/main/java/com/example/appmovil/Home_Admin.java
+++ app/src/main/java/com/example/appmovil/Home_Admin.java
@@ -1,41 +1,209 @@
 package com.example.appmovil;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Home_Admin extends AppCompatActivity implements View.OnClickListener {
+import com.example.appmovil.Adapter.Clientes;
+import com.example.appmovil.Adapter.ClientesAdaptador;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.Api;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class Home_Admin extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
+
+    /*
     private RecyclerView solicitudes;
     private Button detalleSolicitud;
+     */
+
+    RecyclerView recyclerCliente;
+    ClientesAdaptador clientesAdaptador;
+
+    LinearLayout clienteLinearLayout;
+
+    private GoogleSignInClient mGoogleSignInClient;
+
+    private GoogleApiClient googleApiClient;
+
+    private String nombreUsuario, correoUsuario, idUsuario;
+
+    private TextView nombreTextView;
+
+    private Button buttonCerrarSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_admin);
 
+        /*
         solicitudes = findViewById(R.id.recyclerViewSolicitudes);
         detalleSolicitud = findViewById(R.id.buttonDetalleSolicitud);
         detalleSolicitud.setOnClickListener(this);
         solicitudes.setOnClickListener(this);
+
+         */
+        inicializarElementos();
+
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this,this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        nombreTextView = findViewById(R.id.nombreTextView);
+        buttonCerrarSesion = findViewById(R.id.buttonCerrarSesion);
+        buttonCerrarSesion.setOnClickListener(this);
+    }
+
+    private void inicializarElementos() {
+        recyclerCliente = findViewById(R.id.recyclerViewSolicitudes);
+        recyclerCliente.setLayoutManager(new LinearLayoutManager(this));
+
+        List<Clientes> clientesList = new ArrayList<>();
+
+        for (int i=0; i<10; i++){
+            clientesList.add(new Clientes(i,nombreUsuario,correoUsuario+", "+idUsuario));
+        }
+
+        clientesAdaptador = new ClientesAdaptador(clientesList,this);
+
+        recyclerCliente.setAdapter(clientesAdaptador);
+
+//        clienteLinearLayout = findViewById(R.id.clienteLL);
+//        clienteLinearLayout.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(googleApiClient);
+        if (opr.isDone()){
+            GoogleSignInResult result = opr.get();
+            handleSignInResut(result);
+        }
+        else {
+            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+                /*
+                @Override
+                public void onResult(@NonNull @org.jetbrains.annotations.NotNull GoogleSignInResult googleSignInResult) {
+                    handleSignInResut(googleSignInResult);
+                }
+                */
+
+                @Override
+                public void onResult(@NonNull GoogleSignInResult googleSignInResult) {
+                    handleSignInResut(googleSignInResult);
+                }
+
+            });
+        }
+
+    }
+
+    private void handleSignInResut(GoogleSignInResult result) {
+        if (result.isSuccess()){
+            GoogleSignInAccount account = result.getSignInAccount();
+
+            nombreUsuario = account.getDisplayName().toString();
+            correoUsuario = account.getEmail().toString();
+            idUsuario = account.getId().toString();
+
+            nombreTextView.setText(account.getDisplayName());
+        }
+        else {
+            goLogInScreen();
+        }
+    }
+
+    private void goLogInScreen() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.buttonDetalleSolicitud:
-                Intent intent = new Intent(Home_Admin.this,Detalle_Solicitud.class);
-                startActivity(intent);
+//                Intent intent = new Intent(Home_Admin.this,Detalle_Solicitud.class);
+//                startActivity(intent);
                 break;
             case R.id.recyclerViewSolicitudes:
-                Intent intent1 = new Intent(Home_Admin.this,Detalle_Solicitud.class);
-                startActivity(intent1);
+//                Intent intent1 = new Intent(Home_Admin.this,Detalle_Solicitud.class);
+//                startActivity(intent1);
+                break;
+            case R.id.clienteLL:
+//                Intent intent2 = new Intent(Home_Admin.this, Detalle_Solicitud.class);
+//                startActivity(intent2);
+                break;
+            case R.id.buttonCerrarSesion:
+                logOut();
                 break;
         }
+    }
+
+    private void logOut() {
+        Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                if (status.isSuccess()){
+                    goLogInScreen();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "No se pudo cerrar sesión",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+/*
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if (keyCode == event.KEYCODE_BACK){
+            Intent intent = new Intent(Home_Admin.this, MainActivity.class);
+            startActivity(intent);
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+*/
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
 }
--- app/src/main/java/com/example/appmovil/MainActivity.java
+++ app/src/main/java/com/example/appmovil/MainActivity.java
@@ -1,22 +1,50 @@
 package com.example.appmovil;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.Api;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.Task;
+
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final int RC_SIGN_IN = 00;
     private EditText editTextUser;
     private EditText editTextPassword;
     private Button buttonSingIn;
     private Button buttonSingUp;
     private Button buttonForgetPassword;
     private TextView textViewMensaje;
+
+//    private GoogleApiClient googleApiClient;
+    private GoogleSignInClient mGoogleSignInClient;
+    private SignInButton signInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +59,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonSingUp.setOnClickListener(this);
         buttonForgetPassword.setOnClickListener(this);
         textViewMensaje = findViewById(R.id.textViewMensaje);
+
+ //       GoogleSingInOptions gso = new GoogleSingnInOptions.Builder(GoogleSignInOptions.DEFAULT_SING_IN).requestEmail().build();
+ //       googleApiClient = new GoogleApiClient.Builder(this).enableAutoManager(this,this).addApi(Auth.GOOGLE_SIGN_IN,gso).build();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+/*
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, (GoogleApiClient.OnConnectionFailedListener) this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+*/
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        signInButton = (SignInButton) findViewById(R.id.signInButton);
+        signInButton.setOnClickListener(this);
+/*
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(intent,RC_SIGN_IN);
+            }
+        });
+        */
     }
     String usuario1 = "cliente123";
     String password1 = "12345";
     String usuario2 = "admin123";
     String password2 = "123456";
+
+    /*
+    @Override
+    protected void onStart() {
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+    //    GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+    //    updateUI(account);
+        super.onStart();
+    }
+    */
 
     @Override
     public void onClick(View v) {
@@ -62,7 +127,78 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Intent intent2 = new Intent(MainActivity.this, Olvido_Password.class);
                 startActivity(intent2);
                 break;
+
+            case R.id.signInButton:
+                signIn();
+        //        Intent intent4 = Auth.GoogleSignInApi.getSignInIntent(mGoogleSignInClient);
+        //        startActivityForResult(intent4,RC_SIGN_IN);
+                break;
+
         }
 
     }
+
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+//        startActivity(signInIntent);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            hadleSignInResult(result);
+
+/*
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+        //    handleSignInResult(task);
+
+            if (task.isSuccessful()){
+                GoogleSignInAccount acct = task.getResult();
+            }
+            else {}
+*/
+
+        }
+    }
+
+    private void hadleSignInResult(GoogleSignInResult result) {
+        if (result.isSuccess()){
+            goMainScreen();
+        }
+        else {
+            Toast.makeText(this, "Login fallido",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private void goMainScreen(){
+        Intent intent = new Intent(this, Home_Admin.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+/*
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+    //        updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("TAG", "signInResult:failed code=" + e.getStatusCode());
+    //        updateUI(null);
+        }
+    }
+
+    */
 }
